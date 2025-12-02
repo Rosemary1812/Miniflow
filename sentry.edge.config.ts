258df531e33440aafd11ee8ce45028bdaf8edc3d
Rosemary1812 <wsmdmy607@qq.com>
2025-12-02 21:27:58 +0800

    feat:Sentry error tracking --to fix

---
+++ sentry.edge.config.ts
@@ -0,0 +1,27 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: 'https://b04707cc05bb46b29e11796ee534dce7@o4510465227489280.ingest.us.sentry.io/4510465311834112',
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+  integrations: [
+    // Add the Vercel AI SDK integration to sentry.server.config.ts
+    Sentry.vercelAIIntegration({
+      recordInputs: true,
+      recordOutputs: true,
+    }),
+    Sentry.consoleLoggingIntegration({ levels: ['log', 'warn', 'error'] }),
+  ],
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -6,32 +6,55 @@ import { createOpenAI } from '@ai-sdk/openai';
 import { createAnthropic } from '@ai-sdk/anthropic';
 import { deepseek } from '@ai-sdk/deepseek';
 
-// const google = createGoogleGenerativeAI();
+const google = createGoogleGenerativeAI();
 // const openai = createOpenAI();
 // const anthropic = createAnthropic();
 export const execute = inngest.createFunction(
   { id: 'execute' },
   { event: 'execute/ai' },
   async ({ event, step }) => {
+    await step.sleep('pretend', '5s');
+    console.warn('Something is missing');
+    console.error('This is error');
     const { steps: deepseekSteps } = await step.ai.wrap('deepseek-generate-text', generateText, {
       model: deepseek('deepseek-chat'),
       system: 'you are a helpful assisstant',
       prompt: 'what is 22*22?',
+      experimental_telemetry: {
+        isEnabled: true,
+        recordInputs: true,
+        recordOutputs: true,
+      },
     });
     // const { steps: geminiSteps } = await step.ai.wrap('gemini-generate-text', generateText, {
     //   model: google('models/gemini-1.5-flash'),
     //   system: 'you are a helpful assisstant',
     //   prompt: 'what is 22*22?',
+    //   experimental_telemetry: {
+    //     isEnabled: true,
+    //     recordInputs: true,
+    //     recordOutputs: true,
+    //   },
     // });
     // const { steps: openaiSteps } = await step.ai.wrap('openai-generate-text', generateText, {
     //   model: openai('gpt-4o-mini'),
     //   system: 'you are a helpful assisstant',
     //   prompt: 'what is 22*22?',
+    //     experimental_telemetry: {
+    //   isEnabled: true,
+    //   recordInputs: true,
+    //   recordOutputs: true,
+    // },
     // });
     // const { steps: anthropicSteps } = await step.ai.wrap('anthropic-generate-text', generateText, {
     //   model: anthropic('claude-3-opus-20240229'),
     //   system: 'you are a helpful assisstant',
     //   prompt: 'what is 22*22?',
+    //     experimental_telemetry: {
+    //   isEnabled: true,
+    //   recordInputs: true,
+    //   recordOutputs: true,
+    // },
     // });
     return {
       deepseekSteps,
